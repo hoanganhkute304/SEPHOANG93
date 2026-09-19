0097aadc43fd4cec4482c1288c596f796c277eeb
hoanganhkute304 <hoanganh3042003@gmail.com>
2026-09-20 01:53:56 +0700

    Cập nhật Bot Token và Chat ID cho Telegram

--- src/lib/firebase.ts
+++
@@ -1,9 +0,0 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
-import config from "../../firebase-applet-config.json";
-
-const app = initializeApp(config);
-export const db = getFirestore(app, config.firestoreDatabaseId);
-export const auth = getAuth(app);
-export const provider = new GoogleAuthProvider();
